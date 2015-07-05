9892f520fcc4a1c5bcdf9a453c2e0fd91aa28382
Richer Larivière <richerlariviere@gmail.com>
2015-07-05 14:56:00 -0400

    Login validation and insert before

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var headerNav = document.getElementsByClassName('header-nav')[0];
-var li = document.createElement('li');
-li.setAttribute('class', 'header-nav-item');
-var link = document.createElement('a');
-link.setAttribute('class', 'header-nav-link');
-var linkText = document.createTextNode('Explore');
-link.appendChild(linkText);
-link.setAttribute('href', '/explore');
-li.appendChild(link);
-headerNav.appendChild(li);
+if(document.getElementsByClassName('logout-form').length > 0){
+    var headerNav = document.getElementsByClassName('header-nav')[0];
+    var li = document.createElement('li');
+    li.setAttribute('class', 'header-nav-item');
+    var link = document.createElement('a');
+    link.setAttribute('class', 'header-nav-link');
+    var linkText = document.createTextNode('Explore');
+    link.appendChild(linkText);
+    link.setAttribute('href', '/explore');
+    li.appendChild(link);
+    headerNav.insertBefore(li, headerNav.childNodes[0]);
+}
